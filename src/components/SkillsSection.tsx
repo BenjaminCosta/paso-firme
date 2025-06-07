10bdd1caac39517952625630e5c7275a34bb9b31
Benjamín Costa Mihanovich <benjacostm100@gmail.com>
2025-06-07 19:55:44 -0300

    img2

--- src/components/SkillsSection.tsx
+++ src/components/SkillsSection.tsx
@@ -16,7 +16,7 @@ const SkillsSection = () => {
       <div
         className="absolute inset-0 z-0"
         style={{
-          backgroundImage: `linear-gradient(rgba(255, 255, 255, 0.5), rgba(227, 216, 204, 0.1)), url('https://images.unsplash.com/photo-1629360035258-2ccb13aa3679?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D')`,
+          backgroundImage: `linear-gradient(rgba(0, 0, 0, 0.4), rgba(0, 0, 0, 0.6)), url('https://images.unsplash.com/photo-1629360035258-2ccb13aa3679?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D')`,
           backgroundSize: "cover",
           backgroundPosition: "center",
           backgroundAttachment: "fixed",
